7bb10ea3bb7aa517c26f4a526cd8a4badc8a8b23
compuattam <compuattam@gmail.com>
2023-01-31 23:03:00 -0500

    Ejercicio 03

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -2,39 +2,69 @@
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
 function sonIguales(x, y) {
-   // Retorna True si "x" e "y" son iguales.
+   // Retorna True si "x" e "y" son iguales. 
    // De lo contrario, retorna False.
    // Tu código:
+   if  (x === y) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function tienenMismaLongitud(str1, str2) {
-   // Retorna True si los dos strings tienen la misma longitud.
+   // Retorna True si los dos strings tienen la misma longitud. 
    // De lo contrario, retorna False.
    // Tu código:
+   if (str1.length === str2.length) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function menosQueNoventa(num) {
-   // Retorna True si el argumento "num" es menor que noventa.
+   // Retorna True si el argumento "num" es menor que noventa. 
    // De lo contrario, retorna False.
    // Tu código:
+   if (num < 90) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function mayorQueCincuenta(num) {
-   // Retorna True si el argumento "num" es mayor que cincuenta.
+   // Retorna True si el argumento "num" es mayor que cincuenta. 
    // De lo contrario, retorna False.
    // Tu código:
+   if (num > 50) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function esPar(num) {
-   // Retorna True si "num" es par.
+   // Retorna True si "num" es par. 
    // De lo contrario, retorna False.
    // Tu código:
+   if (num % 2 == 0) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function esImpar(num) {
-   // Retorna True si "num" es impar.
+   // Retorna True si "num" es impar. 
    // De lo contrario, retorna False.
    // Tu código:
+   if (num % 2 == 1) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
